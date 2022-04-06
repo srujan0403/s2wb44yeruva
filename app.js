3c70e4082a234af1226b0e7c8607dce14b7ab179
srujan0403 <s546444@nwmissouri.edu>
2022-04-06 17:39:12 -0500

    commit

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var icemagicRouter = require('./routes/icemagic');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -21,7 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/icemagic', icemagicRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
